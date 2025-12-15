12d52ef6d6323f3de39295488ddd1cfcd7c499c2
Sanchit Ram Arvind <sanchitram@gmail.com>
2025-12-15 10:30:06 +0530

    preserve old key, use new keys + make everything text

--- pipeline/main.py
+++ pipeline/main.py
@@ -1,18 +1,17 @@
 from pipeline.extract import extract_all
-
-# from pipeline.transformed import transform
-# from pipeline.load import load_olap
+from pipeline.load import load_olap
+from pipeline.transform import transform
 
 
 def main():
     # First extract all
     raw_data = extract_all()
 
-    # TODO: then transform
-    # transformed_data = transform(raw_data)
+    # then transform
+    transformed_data = transform(raw_data)
 
-    # TODO: then load
-    # load_olap(transformed_data)
+    # then load
+    load_olap(transformed_data)
 
 
 if __name__ == "__main__":
